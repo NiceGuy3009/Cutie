8ffddef8237dead51870d4ce5e43fdd3493e5304
NiceGuy30 <152365995+NiceGuy30@users.noreply.github.com>
2023-12-01 13:39:38 +0800

    Update Self.py

--- Self.py
+++ Self.py
@@ -1,13 +1,23 @@
+import requests 
 import streamlit as st
+from streamlit_lottie import st_lottie
 
 # Find more imojis here: https://www.webfx.com/tools/emoji-cheat-sheet/ 
 st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
+
+def load_lottieurl(url):
+   r = requests.get(url)
+   if r.status_code != 200:\
+        return None
+   return r.json()
+# ---- LOAD ASSETS ----
+lottie_coding =  load_lottieurl("https://iconscout.com/lottie-animation/man-doing-packing-for-the-trip-4006802")
 
 # ----HEADER SECTION----
 with st.container():  
     st.subheader("Hi, I am Algen Marc :wave:")
     st.title("A Student of BSCPE 1B")
-    st.write("I am here to show you the beauty of siargao.")
+    st.write("I am here to show you the beauty of SIARGAO.")
 
 # ---- WHAT TO KNOW ----
 with st.container():
@@ -18,10 +28,10 @@ with st.container():
       st.write("##")
       st.write(
          """ 
-        ABOUT;\n
+         ABOUT;\n
          -CULTURE & FOOD\n
          -TOP EVENTS & FESTIVALS\n
-        THINGS YOU CAN DO IN SIARGAO;\n
+        THINGS YOU CAN DO IN SIARGAO\n)
          -SURFING\n
          -SUNRISE AND SUNSETS\n
          -ISLAND HOPPING\n
@@ -34,3 +44,6 @@ with st.container():
          -LOCAL COMMUNITY MARKET 
          """
       )
+   with right_column:
+      st_lottie(lottie_coding, height=300, key="coding")
+      
